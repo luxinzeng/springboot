c94822ac43656c9320f1d552673c697a0eb3ab0a
luxinzeng <luxinzeng0510@outlook.com>
2026-07-10 09:39:33 +0800

    这是关于20260710的Spring-boot的相关提交

---
+++ springboot-init/src/main/java/com/itheima/springbootinit/Person.java
@@ -0,0 +1,45 @@
+package com.itheima.springbootinit;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "person")//使用这个注解需要注意，记得后面添加前缀
+public class Person {
+
+    private String name;
+    private int age;
+    private String[] address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String[] getAddress() {
+        return address;
+    }
+
+    public void setAddress(String[] address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ springboot-redis/src/main/java/com/itheima/springbootredis/SpringbootRedisApplication.java
@@ -0,0 +1,17 @@
+package com.itheima.springbootredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+
+@SpringBootApplication
+public class SpringbootRedisApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRedisApplication.class, args);
+    }
+
+}
